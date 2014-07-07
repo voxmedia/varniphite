649a224c0005066e755b7db883431eb59dc1b216
José de Paula Eufrásio Júnior <jose.junior@gmail.com>
2014-07-07 15:57:37 -0400

    Initial code

---
+++ varniphite.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/marpaia/graphite-golang"
+	"log"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
+)
+
+var (
+	graphiteHost string
+	graphitePort int
+	metricPath   string
+)
+
+func init() {
+	flag.StringVar(&graphiteHost, "H", "localhost", "Hostname for graphite")
+	flag.IntVar(&graphitePort, "p", 2003, "Port for graphite")
+	flag.StringVar(&metricPath, "m", "varnish.stats", "Metric path")
+}
+
+func main() {
+
+	if len(os.Args) < 4 {
+		flag.Usage()
+	}
+	flag.Parse()
+
+	// Read results from varnishstat
+	out, err := exec.Command("varnishstat", "-j").Output()
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	var data interface{}
+	err = json.Unmarshal(out, &data)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	// Deal with this horrible json
+	cleanData := make(map[string]float64)
+	d := data.(map[string]interface{})
+
+	for k, v := range d {
+		switch i := v.(type) {
+		case string:
+			if strings.Contains(i, "timestamp") {
+				continue
+			}
+		case map[string]interface{}:
+			for sk, s := range i {
+				switch si := s.(type) {
+				case float64:
+					if strings.Contains(sk, "value") {
+						cleanData[k] = si
+					}
+				}
+			}
+		default:
+			log.Println("nope")
+		}
+	}
+
+	// Graphite connection and sending
+	conn, err := graphite.NewGraphite(graphiteHost, graphitePort)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	re, err := regexp.Compile("\\(.*\\)")
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	for z, w := range cleanData {
+		z := strings.ToLower(re.ReplaceAllString(z, ""))
+		metric := fmt.Sprintf("%s.%s", metricPath, z)
+		value := fmt.Sprintf("%f", w)
+		conn.SimpleSend(metric, value)
+		log.Printf("Sending %s %s", metric, value)
+	}
+
+}
